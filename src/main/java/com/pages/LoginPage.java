6aea17f036c60aed06c247af32fa64bb00228a84
Mallikarjun K <59144692+arjunkambar@users.noreply.github.com>
2021-05-02 22:40:14 +0530

    Update LoginPage.java

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -32,7 +32,7 @@ public class LoginPage extends TestBase {
 	WebElement Loader;
 
 	public void Login_Page() throws InterruptedException {
-		TestBase.clickable_wait(10, Signin_Button);
+		TestBase.clickable_wait(20, Signin_Button);
 		Signin_Button.click();
 		Email.sendKeys(TestBase.prop.getProperty("username"));
 		button.click();
