83612232cc195df5a766fdcd1d49c961223edfa0
Mallikarjun K <59144692+arjunkambar@users.noreply.github.com>
2021-05-03 10:30:26 +0530

    Update LoginPage.java

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -50,7 +50,7 @@ public class LoginPage extends TestBase {
 	public void verify_username() throws InterruptedException {
 		Thread.sleep(2000);
 		String title = Signin_Button.getText();
-		System.out.println(title);
+		//System.out.println(title);
 		boolean signin = title.equals("Sign Up/Log In");
 		Assert.assertFalse("Not logged-in", signin);
 	}
