9c2668b7621400061bd9448b3af3c894d8b2bc38
Mallikarjun K <59144692+arjunkambar@users.noreply.github.com>
2021-06-25 19:54:51 +0530

    Update TestBase.java

--- src/main/java/com/utility/TestBase.java
+++ src/main/java/com/utility/TestBase.java
@@ -31,7 +31,9 @@ public class TestBase {
 		String browser = prop.getProperty("browser");
 		if (browser.equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.silentOutput", "true");
-			WebDriverManager.chromedriver().setup();
+			String path = System.getProperty("user.dir");
+			System.setProperty(path + "/Drivers/chromedriver");
+			//WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		} else if (browser.equalsIgnoreCase("firefox")) {
 			System.setProperty("webdriver.chrome.silentOutput", "true");
