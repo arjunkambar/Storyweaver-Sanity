7ca13eab5fcbe7daad7f9d3077c2ca2e27e47170
Mallikarjun K <59144692+arjunkambar@users.noreply.github.com>
2021-06-26 20:23:24 +0530

    Update TestBase.java

--- src/main/java/com/utility/TestBase.java
+++ src/main/java/com/utility/TestBase.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
